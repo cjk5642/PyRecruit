41ef9921a10fefebc7d1121e6d83143c2d995cd3
cjk5642 <collin.graduate@gmail.com>
2022-05-17 17:32:01 -0400

    finish player class

--- recruits/ncaaf/player.py
+++ recruits/ncaaf/player.py
@@ -1,7 +1,11 @@
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
 from tqdm import tqdm
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+}
 
 class Players:
     def __init__(self, 
@@ -9,16 +13,18 @@ class Players:
                  institution:str = "HighSchool", 
                  pos:str = None, 
                  composite_rankings:bool = True, 
-                 state:str = None):
+                 state:str = None,
+                 top:int = 1000):
 
         self.year = 2022 if not year else year
         self.institution = institution
         self.pos = self._check_position(pos) if pos else None
+        self.top = top
         self.composite_rankings = composite_rankings
         self.state = state
         self.url = self._create_url()
         self.html_players = self._parse_players()
-        self.players = self._filter_players()
+        self.dataframe = self._filter_players()
 
     def _check_position(self, pos:str):
         positions = ["QB", "RB", "WR", "TE", "OT", "IOL", "EDGE", "DL", "LB", "CB", "S", "ATH", "K", "P", "LS", "RET"]
@@ -49,14 +55,33 @@ class Players:
         return join_base_str
 
     def _parse_players(self):
-        HEADERS = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
-        }
-        page = requests.get(self.url, headers=HEADERS)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        players = soup.findAll("li", class_ = "rankings-page__list-item")
+        all_players = []
+        print("Parsing players...")
+        new_url = self.url + f"&Page=1"
+        page = requests.get(new_url, headers = HEADERS)
+        soup = BeautifulSoup(page.content, "html.parser")
+        players = soup.findAll("li", class_ = "rankings-page__list-item")[:-1]
         players = [p.find('div', class_ = "wrapper") for p in players]
-        return players
+        all_players.extend(players)
+
+        num_players = len(players)
+        i = 2
+        pbar = tqdm(total = self.top - num_players)
+        while num_players < self.top:
+            new_url = self.url + f"&Page={i}"
+            page = requests.get(new_url, headers = HEADERS)
+            soup = BeautifulSoup(page.content, "html.parser")
+            players = soup.findAll("li", class_ = "rankings-page__list-item")[:-1]
+            for p in players:
+                p = p.find('div', class_ = "wrapper")
+                if num_players < self.top:
+                    num_players += 1
+                    pbar.update(1)
+                    all_players.append(p)
+                else:
+                    break
+            i += 1
+        return all_players
 
     def _get_ranking(self, player: str):
         try:
@@ -145,5 +170,84 @@ class Players:
         return pd.DataFrame.from_dict(players)
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, name_id:str):
+        """name_id found from 247 sports player page like Travis-Hunter-46084728"""
+        self.name_id = name_id
+        
+    def _parse_player(self):
+        url = f"https://247sports.com/Player/{self.name_id}/"
+        page = requests.get(url, headers=HEADERS)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        self.recruit_name = soup.find("h1", class_ = "name").text 
+        # find metrics
+        self.pos, self.height, self.weight = self._find_metrics(soup)
+
+        # find details
+        self.high_school, self.city, self.state, self.class_year = self._find_details(soup)
+        
+        # get rankings
+        self.composite_score, self.national_composite_rank, \
+        self.position_composite_rank, self.state_composite_rank, \
+        self.normal_score, self.national_normal_rank, \
+        self.position_normal_rank, self.state_normal_rank = self._find_ratings(soup, self.pos, self.state)
+
+    def _find_metrics(self, soup_page):
+        metrics = soup_page.find("ul", class_ = "metrics-list").find_all("li")
+        for m in metrics:
+            spans =  m.find_all("span")
+            if spans[0].text == 'Pos':
+                pos = spans[1]
+            elif spans[0].text == "Height":
+                height = spans[1]
+            else:
+                weight = spans[1]
+        return pos, height, weight
+
+    def _find_details(self, soup_page):
+        details = soup_page.find("ul", class_ = "details").find_all("li")
+        for d in details:
+            spans = d.find_all("span")
+            if spans[0].text == 'High School':
+                hs = spans[1].find("a").text
+            elif spans[0].text == 'City':
+                city, state = spans[1].text.split(", ")
+            elif spans[0].text == "Class":
+                cls = spans[1].text
+        return hs, city, state, cls
+
+    def _find_ratings(self, soup_page, pos, state):
+        ratings_sections = soup_page.find_all("section", class_ = "rankings-section")
+        for rs in ratings_sections:
+            title = rs.find("h3")
+            if title == "247Sports Composite":
+                ranking = rs.find("div", class_ = "ranking")
+                composite_score = ranking.find("div", class_ = "rank-block")
+                rank_list = rs.find("ul", class_ = "ranks-list").find_all("li")
+                for rl in rank_list:
+                    rank_name, rank_value = rl.find("b").text, rl.find("a").find("strong").text
+                    if rank_name == 'Natl.':
+                        national_composite_rank = rank_value
+                    elif rank_name == pos:
+                        position_composite_rank = rank_value
+                    else:
+                        state_composite_rank = rank_value
+            else:
+                ranking = rs.find("div", class_ = "ranking")
+                normal_score = ranking.find("div", class_ = "rank-block")
+                rank_list = rs.find("ul", class_ = "ranks-list").find_all("li")
+                for rl in rank_list:
+                    rank_name, rank_value = rl.find("b").text, rl.find("a").find("strong").text
+                    if rank_name == 'Natl.':
+                        national_normal_rank = rank_value
+                    elif rank_name == pos:
+                        position_normal_rank = rank_value
+                    else:
+                        state_normal_rank = rank_value
+        return  composite_score, national_composite_rank, \
+                position_composite_rank, state_composite_rank, \
+                normal_score, national_normal_rank, \
+                position_normal_rank, state_normal_rank
+
+                    
+            
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-from recruits.ncaaf.player import Players
-players = Players(year = 2000, pos = "QB")
-print(players.players)
+import requests
+from bs4 import BeautifulSoup
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+}
+page = requests.get("https://247sports.com/Player/Travis-Hunter-46084728/", headers = HEADERS)
+soup = BeautifulSoup(page.content, 'html.parser')
+details = soup.find("ul", class_ = "details").find_all("li")
+for d in details:
+    spans = d.find_all("span")
+    print(spans[1].find('a').text)
+    break
