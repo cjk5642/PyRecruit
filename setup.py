3d5c8b5b924d3888a6d9623d6842c62f325ebf10
cjk5642 <collin.graduate@gmail.com>
2022-05-21 19:47:36 -0400

    create requirements and setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='PyRecruit',
+      version='0.1',
+      description='PyRecruit: Python API to extract College Basketball and Football Recruits',
+      author='Collin J. Kovacs',
+      url='https://github.com/cjk5642/PyRecruit',
+      packages=['recruits'],
+)
